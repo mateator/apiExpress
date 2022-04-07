8a6677f9636eed9d93a33645d62ff90ad03a3c5f
mateator <mateatorss@gmail.com>
2022-04-07 23:12:40 +0200

    filter by text

--- routes/people.js
+++ routes/people.js
@@ -1,7 +1,9 @@
-const { text } = require("body-parser");
 var express = require("express");
 var router = express.Router();
 var cors = require('cors')
+
+  //podia haberme creado una BD con mySQL y prisma para hacer consultas a las tablas pero me consumiría mucho tiempo y tendría que levantar la BD en local etc
+  //he empleado los 2 JSON como objetos y como nunca lo he hecho antes, por curiosidad, he hecho las queries de SQL mediante JS
 
 router.post("/",cors(), function (req, res, next) {
   const populationData = require("../public/mockData/info-population.json");
@@ -28,7 +30,6 @@ router.post("/",cors(), function (req, res, next) {
           //gender
           case 0:
             if (person.sex === data.key) {
-              // console.log(1);
               person.sex = data;
             }
             break;
@@ -47,9 +48,14 @@ router.post("/",cors(), function (req, res, next) {
     });
   });
 
-  //pagination
-  const data = populationData.population.person.filter(person => console.log(person.filter(p=>p.includes("2000")).length) );
-  const paginatedData = data.splice(req.body.offset,req.body.limit);
+  //itero el array de personas, de cada cual saco los attributos de cada objeto transformados en array para poder hacer .some 
+  //con el .some comparo cada objeto que contiene una person, al emplear some cuando encuentre una coincidencia dejará de buscar
+  //si no encuentra ninguna coincidencia en los que son != object, se va al else y busca dentro de los objetos obtenidos con los inner join
+  //si la condicion principal y el else devuelven false, const data no almacenará el valor que no queremos encontrar con el filtro
+  const data = populationData.population.person.filter(person =>  Object.values(person).some(personObjectData => typeof personObjectData != 'object' ?
+   personObjectData.toString().toLowerCase().includes(req.body.textToFilter.toLowerCase()) : Object.values(personObjectData).some(personObjectObjectData => personObjectObjectData.toString().toLowerCase().includes(req.body.textToFilter.toLowerCase()))));
+
+   const paginatedData = data.splice(req.body.offset,req.body.limit);
 
   res.send(paginatedData);
 });
